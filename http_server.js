0a976452ef7acc5f008a9a270c790ebc56328914
masonjake <jakemason543@gmail.com>
2023-08-21 13:03:43 -0400

    fixed faker error

--- http_server.js
+++ http_server.js
@@ -5,7 +5,7 @@ const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db = low(adapter);
 const cors = require('cors');
-const { faker } = require('@faker-js/faker');
+const faker = require('@faker-js/faker');
 
 // allow cross-origin resource sharing (CORS)
 app.use(cors());
@@ -26,31 +26,31 @@ let port = process.env.PORT || 3000;
 
 // return all users
 app.get('/data', function (req, res) {
-    res.send(db.get('users').value());
+	res.send(db.get('users').value());
 });
 
 // add user
 app.post('/add', function (req, res) {
-    var user = {
-        'name': req.body.name,
-        'dob': req.body.dob,
-        'email': req.body.email,
-        'username': req.body.username,
-        'password': req.body.password,
-        'phone': req.body.phone,
-        'streetaddress': req.body.streetaddress,
-        'citystatezip': req.body.citystatezip,
-        'latitude': req.body.latitude,
-        'longitude': req.body.longitude,
-        'avatar': faker.internet.avatar() 
-    }
-    db.get('users').push(user).write();
-    console.log(db.get('users').value());
-    res.send(db.get('users').value());
+	var user = {
+		name: req.body.name,
+		dob: req.body.dob,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password,
+		phone: req.body.phone,
+		streetaddress: req.body.streetaddress,
+		citystatezip: req.body.citystatezip,
+		latitude: req.body.latitude,
+		longitude: req.body.longitude,
+		avatar: faker.internet.avatar(),
+	};
+	db.get('users').push(user).write();
+	console.log(db.get('users').value());
+	res.send(db.get('users').value());
 });
 
 // start server
 // -----------------------
 app.listen(port, function () {
-    console.log(`Running on port ${port}`);
+	console.log(`Running on port ${port}`);
 });
